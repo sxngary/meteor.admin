812491522b1f305b6739c56a36aaf268e4ee2e8d
garysxn <gauravsa@smartdatainc.net>
2016-11-09 17:29:01 +0530

    Assessment Management

--- client/components/agreements/main.js
+++ client/components/agreements/main.js
@@ -9,14 +9,14 @@ Template.agreements.onCreated(function(){
 
 Template.agreements.onRendered(function(){
     $('.modal').modal({
-        //dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        //dismissible: false, // Modal can be dismissed by clicking outside of the modal
         //opacity: .5, // Opacity of modal background
         //in_duration: 300, // Transition in duration
         //out_duration: 200, // Transition out duration
-        //starting_top: '4%', // Starting top style attribute
+        //starting_top: '0%', // Starting top style attribute
         //ending_top: '10%', // Ending top style attribute
         //ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-        //  alert("Ready");
+        //  //alert("Ready");
         //  console.log(modal, trigger);
         //},
         //complete: function() { alert('Closed'); } // Callback for Modal close    
