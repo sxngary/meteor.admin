bbfc03fa90c4a39d3f2b38ba4b0ae7a4a9096e19
Gaurav Saxena <gauravsa@smartdatainc.net>
2016-11-11 11:07:26 +0530

    added schema for questionnaires, categories, ques, answers

--- imports/api/questionnaires/answers.js
+++ imports/api/questionnaires/answers.js
@@ -4,3 +4,42 @@ class AnswersCollection extends Mongo.Collection {
 }
 
 export const Answers = new AnswersCollection('answers');
+
+var Schema = {};
+Schema.Answers = new SimpleSchema({
+    answer_text: {
+        type: String,
+        optional: true
+    },
+    answer_value: {
+        type: String,
+        optional: true
+    },
+    question: {
+        type: String,
+        optional: true
+    },
+    rank: {
+        type: Number,
+        optional: true
+    },
+    deleted: {
+        type: String,
+        defaultValue: '0'
+    },
+    created: {
+        type: Date,
+        defaultValue: new Date()
+    },
+    modified: {
+        type: Date,
+        autoValue: function () {
+            if (this.isUpdate) {
+                return new Date();
+            }
+        },
+        denyInsert: true,
+        optional: true
+    }
+});
+Answers.attachSchema(Schema.Answers);
